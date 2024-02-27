063c29b964ca54ba739d2cde90b9b966def3a480
dafne <dafnemolina793@gmail.com>
2024-02-27 20:25:23 -0300

    Agregados app.js, estilos.css, home.html, login.htm y register.html

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname,'./public')));
 
-app.listen(3000,()=>{
-    console.log('servidor corriendo')
-});
-
 app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,',/views/home.html'))
+    res.sendFile(path.resolve(__dirname,'C:/Users/Diego/Desktop/cursodigital/carpeta/views/home.html'))
     
 })
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'C:/Users/Diego/Desktop/cursodigital/carpeta/views/register.html'));
+  
+})
+app.get('/login',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,('C:/Users/Diego/Desktop/cursodigital/carpeta/views/login.htm')))
+})
+app.listen(3001,()=>{
+    console.log('servidor corriendo')
+});
