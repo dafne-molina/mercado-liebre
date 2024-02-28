644405fefa595c21478d8a37f9fff8dd2112bf87
dafne <dafnemolina793@gmail.com>
2024-02-27 23:14:05 -0300

    Mensaje de tu commit aquí

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
+const port = process.env.PORT || 3033;
 const express = require ('express');
+const app = express();
+
 const path = require ('path');
 
-const app = express();
+
 
 app.use(express.static(path.resolve(__dirname,'./public')));
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'C:/Users/Diego/Desktop/cursodigital/carpeta/views/home.html'))
+    res.sendFile(path.resolve(__dirname,'C:\Users\Diego\Desktop\cursodigital\carpeta\views\home.html'))
     
 })
 app.get('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'C:/Users/Diego/Desktop/cursodigital/carpeta/views/register.html'));
+    res.sendFile(path.resolve(__dirname, 'C:\Users\Diego\Desktop\cursodigital\carpeta\views\register.html'));
   
 })
 app.get('/login',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,('C:/Users/Diego/Desktop/cursodigital/carpeta/views/login.htm')))
+    res.sendFile(path.resolve(__dirname,('C:\Users\Diego\Desktop\cursodigital\carpeta\views\login.htm')))
 })
-app.listen(3001,()=>{
-    console.log('servidor corriendo')
-});
+app.listen(port,()=> console.log(`Servidor corriendo en el puerto ${port}`));
+    
