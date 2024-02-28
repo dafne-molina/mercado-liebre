5ce5cb763d5f9e98727e0c6bd72edc3fcac488df
dafne <dafnemolina793@gmail.com>
2024-02-27 23:26:48 -0300

    Actualización en app.js

--- app.js
+++ app.js
@@ -9,15 +9,15 @@ const path = require ('path');
 app.use(express.static(path.resolve(__dirname,'./public')));
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'C:\Users\Diego\Desktop\cursodigital\carpeta\views\home.html'))
+    res.sendFile(path.resolve('views/home.html'))
     
 })
 app.get('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'C:\Users\Diego\Desktop\cursodigital\carpeta\views\register.html'));
+    res.sendFile(path.resolve('views/register.html'));
   
 })
 app.get('/login',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,('C:\Users\Diego\Desktop\cursodigital\carpeta\views\login.htm')))
+    res.sendFile(path.resolve(('views/login.htm')))
 })
 app.listen(port,()=> console.log(`Servidor corriendo en el puerto ${port}`));
     
